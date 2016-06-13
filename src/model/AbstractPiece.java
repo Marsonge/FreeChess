b20aedcb186263f3384a9f7ec2df33365d530299
Marsonge <tim.caillot@gmail.com>
2016-06-13 09:39:44 +0200

    Knight added

--- src/model/AbstractPiece.java
+++ src/model/AbstractPiece.java
@@ -6,18 +6,41 @@ public abstract class AbstractPiece implements Pieces{
 	protected Coord coord;
 	
 	public static void main(String args[]){
+		System.out.println("####TEST DES PIECES####");
+		System.out.println("##ROOK##");
 		Pieces maTour = new Tour(Couleur.NOIR, new Coord(0, 0));
 		System.out.println(maTour.getX());
 		System.out.println(maTour.getY());
 		System.out.println(maTour);
 		maTour.move(3, 0);
 		System.out.println(maTour);
+		//Incorrect move
 		maTour.move(4,7);
 		System.out.println(maTour);
+		//Out of move test
 		maTour.move(3,8);
 		System.out.println(maTour);
 		maTour.move(3,7);
 		System.out.println(maTour);
+		System.out.println("##KNIGHT##");
+		Pieces monCav = new Cavalier(Couleur.NOIR, new Coord(3, 3));
+		System.out.println(monCav.getX());
+		System.out.println(monCav.getY());
+		System.out.println(monCav);
+		monCav.move(4,1);
+		System.out.println(monCav);
+		monCav.move(3,3);
+		System.out.println(monCav);
+		//Incorrect move test
+		monCav.move(4,2);
+		System.out.println(monCav);
+		monCav.move(2,5);
+		monCav.move(1,7);
+		System.out.println(monCav);
+		//Out of bonds test
+		monCav.move(2,9);
+		System.out.println(monCav);
+		
 	}
 	
 	public AbstractPiece(Couleur couleur, Coord coord){
--- src/model/Cavalier.java
+++ src/model/Cavalier.java
@@ -2,4 +2,32 @@ package model;
 
 public class Cavalier extends AbstractPiece{
 
+	public Cavalier(Couleur couleur, Coord coord) {
+		super(couleur, coord);
+	}
+
+	@Override
+	public boolean capture() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getName() {
+		return "Knight";
+	}
+	
+	@Override
+	public boolean isMoveOk(int xFinal, int yFinal, boolean isCatchOk,
+			boolean isCastlingPossible) {
+		int x = this.coord.x;
+		int y = this.coord.y;
+		if((Math.abs(xFinal - x) == 2 && Math.abs(yFinal - y) == 1) || (Math.abs(xFinal - x) == 1 && Math.abs(yFinal - y) == 2)){
+			if(Coord.coordonnees_valides(xFinal, yFinal)){
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
